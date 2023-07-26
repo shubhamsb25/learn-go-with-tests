e1d8855af24a32d495a36fbff4c8d1775c13c869
Shubham Singh Bhaisora <shubhamsb@outlook.com>
2023-07-26 20:24:42 +0530

    add functionality to repeat character by given num

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,19 @@
 package iteration
 
-func Repeat(character string) string {
+// Repeats character string by given times.
+// Defaults to 5 times if provided invalid value
+func Repeat(character string, givenTimes int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	var times int
+
+	if givenTimes <= 0 {
+		times = 5
+	} else {
+		times = givenTimes
+	}
+
+	for i := 0; i < times; i++ {
 		repeated += character
 	}
 	return repeated
 }
-
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,18 +1,49 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeats default five times if given zero", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+		assertExpectedValue(t, expected, repeated)
+	})
+
+	t.Run("repeats default 5 times if negative value", func(t *testing.T) {
+		repeated := Repeat("a", -1)
+		expected := "aaaaa"
+
+		assertExpectedValue(t, expected, repeated)
+	})
+
+	t.Run("repeats given n times", func(t *testing.T) {
+		times := 3
+		repeated := Repeat("a", times)
+		expected := "aaa"
+
+		assertExpectedValue(t, expected, repeated)
+	})
+}
+
+func assertExpectedValue(t *testing.T, expected string, actual string) {
+	t.Helper()
+	if actual != expected {
+		t.Errorf("expected %q but got %q", expected, actual)
 	}
+}
+
+func ExampleRepeat() {
+	repeatedCharacter := Repeat("a", 4)
+	fmt.Println(repeatedCharacter)
+	//Output: aaaa
 }
 
 func Benchmark(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 0)
 	}
 }
